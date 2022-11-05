899827edb06b56d14da6ef0b2996a2aa5b261ed2
vankichi <kyukawa315@gmail.com>
2022-11-05 17:27:59 +0900

    :arrow_up: update deps
    Signed-off-by: vankichi <kyukawa315@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { InsertController } from './insert/insert.controller';
+import { InsertService } from './insert/insert.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CatsController, InsertController],
+  providers: [AppService, InsertService],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ['.env']
+  })],
 })
 export class AppModule {}
